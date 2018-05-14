ba6b99cf4e0be197941c037a3788aad3ef4367ac
JakeBCR <39069587+JakeBCR@users.noreply.github.com>
2018-05-14 16:31:13 -0600

    Update remittance.sol
    changes inc

--- remittance.sol
+++ remittance.sol
@@ -3,13 +3,10 @@ pragma solidity 0.4.19;
 contract remittance {
     
     //storing hashed passwords
-    mapping(bytes32) public passwords;
+    mapping(bytes32) public hashPassword;
     
-    //storing balances
+    //storing balances WIP
     mapping(address => uint) public balances;
-    
-    //setting the destination for the funds (for re-usability)
-    mapping(uint => address) public address;
     
     //logs who sent how much funds to the contract (if any)
     event LogReceiveFunds(uint amount, address sender);
@@ -22,44 +19,33 @@ contract remittance {
     
     //depositing the funds into the contract and setting the receivers value
     
-    function depositFunds(address destination, uint value, bytes32 password1, bytes32 password2, bytes32 password0) public payable {
+    function depositFunds(bytes32 hashPass) public payable {
         require(msg.value > 0);
-        
-        //write the passwords into storage
-        passwords[Owner] = password0;
-        passwords[Bob] = password1;
-        passwords[Carol] = password2;
-        
-        //provide the destination address
-        address[addrCarol] = destination;
-        address[Owner] = msg.sender;
-        
-        //assign the value to destination
+        hashPassword[hash]= hashPass
         balances[receiver1] += msg.value;
     }
     
-    
     function withdrawFunds(bytes32 providedPw1, bytes32 providedPw2) public payable {
         
-        //Hash the provided password
-        keccak256(providedPw1) = providedHash1;
-        keccak256(providedPw2) = providedHash2;
+    //Hash the provided password
+        bytes32 providedHash = keccak256(providedPw1, providedPw2)
         
-        //check passwords and addresses
-        require providedHashB = passwords[Bob];
-        require providedHashC = passwords[Carol];
-        require msg.sender = addrCarol;
-       
-            msg.sender.transfer();
-            
+    //check passwords and addresses
+        require (providedHash = hashPass);
+        msg.sender.transfer();
+    //set hashpass to 0 (no hash can produce 0 outcome so it must be reset by using the deposit funds) WIP
+        hashPass = 0;   
      }
 
     function cancelWithdrawl(bytes32 providedPw0) public payable {
     
-        keccak256(providedPw0) = providedHash0;
-        require providedHash0 = passwords[Owner];
-        require msg.sender = owner;
-        msg.sender.transfer(balances[receiver1]);
+        bytes providedHash = keccak256(providedPw0);
+        require (providedHash0 = hashPass);
+        require (msg.sender = owner);
+        msg.sender.transfer();
+        
+        //WIP
+        hashPass = 0;
     
     }
 }
